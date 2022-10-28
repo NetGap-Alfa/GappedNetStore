5bfe6a1e2beeda7a46d82c087683b60069d15c31
sebastian-ochoa1 <sebastian.ochoa1@udea.edu.co>
2022-10-27 23:36:16 -0500

    Se crea una parte del carrito de compras solo faltando ordenarlo con CSS

---
+++ src/data/data.js
@@ -0,0 +1,186 @@
+export const clientsExample = {
+    "clientes": [
+      {
+        "identificacion": "111111",
+        "apellido": "Smith",
+        "edad": "34",
+        "dirección": {
+          "calle": "21 2nd Street",
+          "ciudad": "New York",
+          "localidad": "NY",
+          "codigo postal": "10021"
+        },
+        "telefono": [
+          {
+            "tipo": "home",
+            "numero": "212 555-1234"
+          },
+          {
+            "tipo": "fax",
+            "numero": "646 555-4567"
+          }
+        ],
+        "nombre": "John",
+        "correo": "ja@prueba.com"
+      },
+      {
+        "identificacion": "22222",
+        "apellido": "Claus",
+        "edad": 25,
+        "dirección": {
+          "calle": "21 2nd Street",
+          "ciudad": "New York",
+          "localidad": "NY",
+          "codigo postal": "10021"
+        },
+        "telefono": [
+          {
+            "tipo": "home",
+            "numero": "313 555-1234"
+          },
+          {
+            "tipo": "fax",
+            "numero": "654 555-4567"
+          }
+        ],
+        "nombre": "Mario",
+        "correo": "mas@prueba.com"
+      }
+    ],
+    "administradores": [
+      {
+        "identificacion": "1231",
+        "apellido": "Moreno",
+        "edad": "30",
+        "dirección": {
+          "calle": "1 2nd Street",
+          "ciudad": "New York",
+          "localidad": "NY",
+          "codigo postal": "10021"
+        },
+        "telefono": [
+          {
+            "tipo": "home",
+            "numero": "212 444-1234"
+          },
+          {
+            "tipo": "fax",
+            "numero": "646 444-4567"
+          }
+        ],
+        "nombre": "Judy",
+        "correo": "jm@prueba.com"
+      },
+      {
+        "identificacion": "2342",
+        "apellido": "Tellez",
+        "edad": 31,
+        "dirección": {
+          "calle": "212 2nd Street",
+          "ciudad": "New York",
+          "localidad": "NY",
+          "codigo postal": "10021"
+        },
+        "telefono": [
+          {
+            "tipo": "home",
+            "numero": "313 555-1298"
+          },
+          {
+            "tipo": "fax",
+            "numero": "654 555-9876"
+          }
+        ],
+        "nombre": "Andres",
+        "correo": "at@prueba.com"
+      }
+    ]
+  }
+
+export const productCartExample = {
+    "productos": [
+      {
+        "id": "GGOEAFKA087499",
+        "urlImage": "https://i.blogs.es/27b569/telefono/450_1000.jpeg",
+        "amount":2,
+        "name": "Android Small Removable Sticker Sheet",
+        "price": "2.99"
+      },
+      {
+        "id": "GGOEAFKA087599",
+        "urlImage": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Rubik's_cube.svg/240px-Rubik's_cube.svg.png",
+        "name": "Android Large Removable Sticker Sheet",
+        "amount":1,
+        "price": "3.99"
+      }
+    ]
+}
+
+export const productsExample = {
+    "productos": [
+      {
+        "id": "GGOEAFKA087499",
+        "urlImagen": "https://i.blogs.es/27b569/telefono/450_1000.jpeg",
+        "nombre": "Android Small Removable Sticker Sheet",
+        "descripcion": "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
+        "características": "8 Android stickers White colored sticker sheet",
+        "precio": "2.99"
+      },
+      {
+        "id": "GGOEAFKA087599",
+        "urlImagen": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Rubik's_cube.svg/240px-Rubik's_cube.svg.png",
+        "name": "Android Large Removable Sticker Sheet",
+        "description": "Show your quirky side by placing these fun Android stickers on your personal belongings.",
+        "features": "Android Stickers White Colored Sticker Sheet",
+        "price": "3.99"
+      }
+    ]
+}
+
+export const sellsExample = {
+    "ventas": [
+      {
+        "fecha": "27/09/2022",
+        "idCliente": "111111",
+        "idVenta": "123",
+        "valor": 35,
+        "confirmado": true,
+        "detalleCompra": [
+          {
+            "idProducto": "GGOEAFKA087599",
+            "cantidad": 2
+          }
+        ]
+      },
+      {
+        "fecha": "28/09/2022",
+        "idCliente": "22222",
+        "idVenta": "234",
+        "valor": 50,
+        "confirmado": false,
+        "detalleCompra": [
+          {
+            "idProducto": "GGOEAFKA087499",
+            "cantidad": 2
+          }
+        ]
+      },
+      {
+        "fecha": "29/09/2022",
+        "idCliente": "1231",
+        "idVenta": "543",
+        "valor": 10,
+        "confirmado": true,
+        "detalleCompra": [
+          {
+            "idProducto": "GGOEAFKA087499",
+            "cantidad": 2
+          },
+          {
+            "idProducto": "GGOEAFKA087599",
+            "cantidad": 1
+          }
+        ]
+      }
+    ]
+  }
--- src/pages/shoppingCart/shoppingCartPage.js
+++ src/pages/shoppingCart/shoppingCartPage.js
@@ -1,16 +1,46 @@
-import React from 'react'
-import './shoppingCartPage.css'
-import { ClientHeader } from '../../components/clientHeader/clientHeader'
+import { useNavigate } from "react-router-dom";
+import React from "react";
+import "./shoppingCartPage.css";
+import { ClientHeader } from "../../components/clientHeader/clientHeader";
+import { Button } from "../../components/button/button";
+import { productCartExample } from "../../data/data";
 
 export const ShoppingCartPage = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      <ClientHeader/>
+      <ClientHeader />
       <div className="main-container">
-        <h1 className='h1-shoppingCartPage'>
-          Shopping Cart PAGE
-        </h1>
+        <table>
+          <tr>
+            <th>Imagen</th>
+            <th>Cantidad</th>
+            <th>Producto</th>
+            <th>Valor</th>
+            <th>Total</th>
+          </tr>
+          {productCartExample.productos.map((product) => (
+            <tr>
+              <td>
+                <img src={product.urlImage}/>
+              </td>
+              <td>{product.amount}</td>
+              <td>{product.name}</td>
+              <td>{product.price}</td>
+              <td>{product.price*product.amount}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+      <div className="button-container">
+        <div className="header-button-container">
+          <Button text="Terminar venta" />
+        </div>
+        <Button
+          text="Cancelar"
+          onClickFunc={() => navigate("/client/shoppingList")}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
