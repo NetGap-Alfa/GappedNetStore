b7e41cc991899a6146bf100496367d36e8716a85
aguilarelkin <aguilar.elkin99@gmail.com>
2022-11-02 09:45:53 -0500

    Cambios css

--- src/components/adminHeader/adminHeader.js
+++ src/components/adminHeader/adminHeader.js
@@ -9,13 +9,13 @@ export const AdminHeader = () => {
 <>
 
 <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
-  <div className="container-fluid">
+  <div className="container-fluid justify-content-end">
     
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0 fs-1">
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href={'/admin/shoppingList'} >Lista productos</a>
         </li>
