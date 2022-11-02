c6eb4394cfd6351bc183205d91089b930b7685fc
ricomariaca <61256274+ricomariaca@users.noreply.github.com>
2022-11-02 17:54:19 -0500

    se corrigio y termino  salesListPages

--- src/pages/salesList/salesListPage.js
+++ src/pages/salesList/salesListPage.js
@@ -4,9 +4,15 @@ import { AdminHeader } from "../../components/adminHeader/adminHeader";
 import { sellsExample } from "../../data/data";
 
 export const SalesListPage = () => {
+  let total = 0
+
+  for (let i = 0; i < sellsExample.ventas.length; i++) {
+
+    total = total + sellsExample.ventas.at(i).valor
+  }
   return (
     <div>
-      <AdminHeader/>
+      <AdminHeader />
       <div className="main-container">
         <h1 className="h1-salesList">Sales List Page</h1>
 
@@ -17,33 +23,19 @@ export const SalesListPage = () => {
             <th className="th-salesList">Valor</th>
           </tr>
           {sellsExample.ventas.map((worth) => (
-
             <tr>
               <td className="th-salesList">{worth.fecha}</td>
               <td className="th-salesList">{worth.idVenta}</td>
               <td className="th-salesList">{worth.valor}</td>
             </tr>
-            
-            
-            
           ))}
-           <tr>
+          <tr>
             <th className="th-salesList"></th>
             <th className="th-salesList">Total</th>
-            <th className="th-salesList">95</th>
+            <th className="th-salesList">{total}</th>
           </tr>
-          
-
         </table>
-       
-            <script>
-              
-            </script>
-
-
       </div>
-
-     
     </div>
   );
 };
