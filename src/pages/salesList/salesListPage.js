755f9a131af16fd43fdac6d1e6aab155801af1e3
ricomariaca <61256274+ricomariaca@users.noreply.github.com>
2022-11-02 01:14:12 -0500

    se realiso Vista Administrador

--- src/pages/salesList/salesListPage.js
+++ src/pages/salesList/salesListPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./salesListPage.css";
 import { AdminHeader } from "../../components/adminHeader/adminHeader";
+import { sellsExample } from "../../data/data";
 
 export const SalesListPage = () => {
   return (
@@ -8,7 +9,41 @@ export const SalesListPage = () => {
       <AdminHeader/>
       <div className="main-container">
         <h1 className="h1-salesList">Sales List Page</h1>
+
+        <table className="table-salesList">
+          <tr>
+            <th className="th-salesList">Fecha</th>
+            <th className="th-salesList">IdVenta</th>
+            <th className="th-salesList">Valor</th>
+          </tr>
+          {sellsExample.ventas.map((worth) => (
+
+            <tr>
+              <td className="th-salesList">{worth.fecha}</td>
+              <td className="th-salesList">{worth.idVenta}</td>
+              <td className="th-salesList">{worth.valor}</td>
+            </tr>
+            
+            
+            
+          ))}
+           <tr>
+            <th className="th-salesList"></th>
+            <th className="th-salesList">Total</th>
+            <th className="th-salesList">95</th>
+          </tr>
+          
+
+        </table>
+       
+            <script>
+              
+            </script>
+
+
       </div>
+
+     
     </div>
   );
 };
