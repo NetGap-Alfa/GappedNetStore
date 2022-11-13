eed968cccd9ed7222324ec7c22fa34ed8556a7f9
sebastian-ochoa1 <sebastian.ochoa1@udea.edu.co>
2022-11-13 17:17:27 -0500

    Se añade bodyparser y tarea agregar productos al carrito

--- routes/shoppingCartRoutes.js
+++ routes/shoppingCartRoutes.js
@@ -3,7 +3,57 @@ const cors = require("cors");
 
 const router = express.Router();
 
-//Datos locales
+//#region Datos locales
+const products = [
+  {
+    id: "GGOEAFKA087499",
+    urlImagen: "https://i.blogs.es/27b569/telefono/450_1000.jpeg",
+    nombre: "Android Small Removable Sticker Sheet",
+    descripcion:
+      "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
+    stock: "40",
+    precio: "2.99",
+  },
+  {
+    id: "GGOEAFKA087599",
+    urlImagen:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Rubik's_cube.svg/240px-Rubik's_cube.svg.png",
+    nombre: "Android Large Removable Sticker Sheet",
+    descripcion:
+      "Show your quirky side by placing these fun Android stickers on your personal belongings.",
+    stock: "10",
+    precio: "3.99",
+  },
+  {
+    id: "GGOEAFKA087399",
+    urlImagen:
+      "https://olimpica.vtexassets.com/arquivos/ids/557166/Celular-XIAOMI-Redmi-Note-10-Pro-128GB-Blue.jpg",
+    nombre: "Android Cellphone",
+    descripcion:
+      "Show your Android Cellphone pride by placing these technology products to the top",
+    stock: "15",
+    precio: "444.99",
+  },
+  {
+    id: "GGOEAFKA087299",
+    urlImagen:
+      "https://spectrum.ieee.org/media-library/the-ibm-pc-introduced-in-august-1981.jpg?id=27044507&width=400&height=300",
+    nombre: "IBM computer",
+    descripcion: "IBM computer, very pricy but old AF",
+    stock: "2",
+    precio: "667.99",
+  },
+  {
+    id: "GGOEAFKA087199",
+    urlImagen:
+      "https://www.akg.com/dw/image/v2/AAUJ_PRD/on/demandware.static/-/Sites-masterCatalog_Harman/default/dw671f8dbf/AKG_HT300_Mic_Rear_RGB_Clipped.png?sw=556&sh=680&sm=fit",
+    nombre: "PNG Microphone",
+    descripcion: "No idea, i just wanted to see how a png would work",
+    stock: "5",
+    precio: "9.99",
+  },
+];
+
 const shoppingCart = {
   productos: [
     {
@@ -23,10 +73,12 @@ const shoppingCart = {
     },
   ],
 };
+//#endregion
 
+//#region GET THE WHOLE CART
 router.get("/", cors(), (req, res) => {
   let totalPrice = 0;
-  
+
   shoppingCart.productos.forEach((product) => {
     totalPrice = totalPrice + product.amount * parseFloat(product.price);
   });
@@ -35,9 +87,37 @@ router.get("/", cors(), (req, res) => {
 
   res.json(shoppingCart);
 });
+//#endregion
 
+//#region ADD PRODUCT TO THE CART
+router.post("/addToCart", cors(), (req, res) => {
+  const product = products.find((product) => product.id == req.body.productId);
+  if (product != undefined) {
+    const index = shoppingCart.productos.findIndex(
+      (product) => product.id == req.body.productId
+    );
+    if (index < 0) {
+      shoppingCart.productos.push({
+        id: product.id,
+        urlImagen: product.urlImagen,
+        name: product.nombre,
+        amount: req.body.amount,
+        price: product.precio,
+      });
+    } else {
+      shoppingCart.productos.find((product) => product.id == req.body.productId).amount = req.body.amount
+    }
+    res.json("Agregado con exito");
+  } else {
+    res.json({ errorMessage: "Not found" });
+  }
+});
+//#endregion
+
+//#region BUY THE CART
 router.post("/buy", cors(), (req, res) => {
   res.json("Comprado con exito");
 });
+//#endregion
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.json())
 
 //Import Routes
 const productRoutes = require('./routes/productRoutes')
