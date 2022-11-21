2c5fa328606d5a662feb82d7c935bdd82da49d80
sebastian-ochoa1 <sebastian.ochoa1@udea.edu.co>
2022-11-21 01:18:34 -0500

    Cambio en URL

--- frontend/src/pages/login/loginPage.js
+++ frontend/src/pages/login/loginPage.js
@@ -1,39 +1,55 @@
+import { useNavigate } from "react-router-dom";
 import React from "react";
-import axios from "axios";
+import { Button } from "../../components/button/button";
 import "./loginPage.css";
 
 export const LoginPage = () => {
-  const [post, setPost] = React.useState(null);
-
-  const baseURL = "/api/customers";
-
-  React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-      console.log(post)
-    });
-  }, []);
+  const navigate = useNavigate();
 
   return (
     <div>
       <div className="main-container">
+        <Button
+          text="Vista ADMIN"
+          onClickFunc={() => navigate("/admin/shoppingList")}
+        />
+        <Button
+          text="Vista CLIENTE"
+          onClickFunc={() => navigate("/client/shoppingList")}
+        />
         <div className="wrapper fadeInDown">
           <div id="formContent">
             <div className="fadeIn first">
-              <img src="https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/man-person-icon.png" id="icon" alt="User Icon" />
+              <img
+                src="https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/man-person-icon.png"
+                id="icon"
+                alt="User Icon"
+              />
             </div>
-            <form>  
-              
-              <label id="user" >Username</label>
-              <input type="text" id="login" className="fadeIn second" name="login" placeholder="mail.com@gmail.com" />
+            <form>
+              <label id="user">Username</label>
+              <input
+                type="text"
+                id="login"
+                className="fadeIn second"
+                name="login"
+                placeholder="mail.com@gmail.com"
+              />
               <label id="user">Password</label>
-              <input type="password" id="login" className="fadeIn third" name="login" placeholder="********" />
+              <input
+                type="password"
+                id="login"
+                className="fadeIn third"
+                name="login"
+                placeholder="********"
+              />
               <input type="submit" className="fadeIn fourth" value="Log In" />
             </form>
             <div id="formFooter">
-              <a className="underlineHover" href="#">Forgot Password?</a>
+              <a className="underlineHover" href="#">
+                Forgot Password?
+              </a>
             </div>
-
           </div>
         </div>
       </div>
