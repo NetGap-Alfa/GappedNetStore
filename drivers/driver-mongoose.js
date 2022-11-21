daf6a4cb0874fd73ecc5d975f4b04be29bc3acf2
Elkin Aguilar <aguilar.elkin99@gmail.com>
2022-11-20 21:38:33 -0500

    Cambios css

--- drivers/driver-mongoose.js
+++ drivers/driver-mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-//const URI = "mongodb://127.0.0.1:27017/netgat"
-const URI = "mongodb+srv://UserDB_MinTIC:faeThjefWFV41IA2@cluster0.ylr66ui.mongodb.net/?retryWrites=true&w=majority"
+const URI = "mongodb://127.0.0.1:27017/netgat"
+//const URI = "mongodb+srv://UserDB_MinTIC:faeThjefWFV41IA2@cluster0.ylr66ui.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(URI)
     .then(()=>console.log(`Connect to mongoose Success..`))
